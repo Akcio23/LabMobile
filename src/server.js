e4dee6f26d495d89b34f77b5d81581adb5b3c502
Akcio23 <dev.akcio@gmail.com>
2025-08-18 20:45:53 -0300

    chore: update server node

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import conectDataBase from './config/database.js';
 import routerUser from './router/routerUser.js';
+import routerRegister from './router/routerRegister.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,9 +14,6 @@ conectDataBase();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }))
 
-
-
-
 app.get('/', (req, res) => {
   res.json({ 
     message: 'API funcionando!', 
@@ -23,10 +21,9 @@ app.get('/', (req, res) => {
   });
 });
 
-
 app.use('/', routerUser);
-
+app.use('/', routerRegister);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Server ON ${PORT}`);
 });
