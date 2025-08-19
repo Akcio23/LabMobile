763d76b13b3e5e3867a76de12e2a2ae476b68743
Akcio23 <dev.akcio@gmail.com>
2025-08-19 19:56:43 -0300

    feat: changes server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import conectDataBase from './config/database.js';
 import routerUser from './router/routerUser.js';
 import routerRegister from './router/routerRegister.js';
+import routerAuthentication from './router/routerAuthentication.js';
+import verifyAuth from './middleware/verifyAuth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,18 +13,21 @@ const PORT = process.env.PORT || 3000;
 conectDataBase();
 
 // Middleware
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.json({ 
-    message: 'API funcionando!', 
-    mongoose: 'Conectado com MongoDB via Mongoose' 
+  res.json({
+    message: 'API funcionando!',
+    mongoose: 'Conectado com MongoDB via Mongoose'
   });
 });
 
-app.use('/', routerUser);
+
 app.use('/', routerRegister);
+app.use('/', routerAuthentication);
+
+app.use('/', verifyAuth, routerUser);
 
 app.listen(PORT, () => {
   console.log(`Server ON ${PORT}`);
