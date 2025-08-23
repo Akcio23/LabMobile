d2fb8edacb699ce730543bdb8d2c1ecb36ab787b
Paulo Henrique de Andrade <plhnrqf@outlook.com>
2025-08-23 18:07:45 -0300

    feat: add Customer model with validations

---
+++ src/models/Customer.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true
+    },
+    cnpjCpf: {
+        type: String,
+        required: [true, 'CPF/CNPJ is required'],
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function(v) {
+                const numbers = v.replace(/\D/g, '');
+                return numbers.length === 11 || numbers.length === 14;
+            },
+            message: 'CPF must have 11 digits or CNPJ must have 14 digits'
+        }
+    },
+    address: {
+        street: { type: String, required: true, trim: true},
+        number: { type: String, required: true, trim: true},
+        neighborhood: { type: String, required: true, trim: true},
+        city: { type: String, required: true, trim: true},
+        state: { type: String, required: true, trim: true},
+        zipCode: { type: String, required: true, trim: true}
+    },
+    phone: {
+        type: String,
+        trim: true,
+        match: [/^\(\d{2}\) \d{4,5}-\d{4}$/, 'Invalid phone format'],
+    },
+    email: {
+        type: String,
+        unique: true,
+        sparse: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email format'],
+    }
+}, {timestamps: true});
+
+const Customer = mongoose.model('Customer', customerSchema);
+export default Customer;
